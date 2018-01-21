f8dd6281e3dc0dd6814979bec89b1f034715bb1a
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-01-22 06:11:03 +0900

    Add project files.

---
+++ InfinityLoop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InfinityLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
